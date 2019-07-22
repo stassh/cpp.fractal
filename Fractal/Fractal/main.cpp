cd7107d95edc1bc8aac0ab5a8d742e1a946f86c6
Shabalin Stanislav <Stanislav.Shabalin@partner.bmw.de>
2019-07-22 16:39:44 +0200

    Add static fractal

--- Fractal/Fractal/main.cpp
+++ Fractal/Fractal/main.cpp
@@ -16,14 +16,95 @@
 
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 // Here is a small helper for you! Have a look.
 #include "ResourcePath.hpp"
 
+const double left = -0.05;
+const double right = 0.05;
+
+const double bottom = -0.05;
+const double top = 0.05;
+
+const double cx = -0.7269;
+const double cy = 0.1889;
+
+// −0.7269 + 0.1889
+
+double normalize(int value, int maxValue, double leftLimit, double rightLimit) {
+    return leftLimit + (rightLimit - leftLimit) * value / maxValue;
+}
+
+sf::Color getColor(int x, int y, int maxX, int maxY) {
+    
+    double zx = normalize(x, maxX, left, right); // scaled x coordinate of pixel # (scaled to lie in the Mandelbrot X scale (−2.5, 1))
+    double zy = normalize(y, maxY, bottom, top); // scaled y coordinate of pixel # (scaled to lie in the Mandelbrot Y scale (−1, 1))
+    
+    int iteration = 0;
+    int max_iteration = 255;
+        
+        while ((zx * zx + zy * zy < 4) && (iteration < max_iteration))
+        {
+            double xtemp = zx * zx - zy * zy;
+            zy = 2 * zx * zy  + cy;
+            zx = xtemp + cx;
+            
+            iteration++;
+        }
+        
+    if (iteration == max_iteration) {
+        return sf::Color::White;
+    }
+    else {
+        sf::Color color;
+        color.r = iteration;
+        return color;
+    }
+}
+
+int render(sf::RenderWindow &window, sf::Sprite sprite, sf::Text text) {
+    // Clear screen
+    window.clear();
+    
+    sf::Vector2u size = window.getSize();
+    int maxX = size.x;
+    int maxY = size.y;
+    std::cout << "Screen size " << size.x << " " << size.y << std::endl;
+    
+    sf::Image image;
+    sf::Color color;
+    
+    image.create(size.x, size.y, sf::Color::White);
+    
+    for (int x = 0; x < maxX; x++) {
+        for (int y = 0; y < maxY; y++) {
+            image.setPixel(x, y, getColor(x, y, maxX, maxY));
+        }
+    }
+    
+    sf::Texture texture;
+    texture.loadFromImage(image);
+    sf::Sprite s(texture);
+    
+    // Draw the sprite
+    // window.draw(sprite);
+    window.draw(s);
+    // sprite.getTexture();
+    
+    // Draw the string
+    window.draw(text);
+    
+    // Update the window
+    window.display();
+    
+    return EXIT_SUCCESS;
+}
+
 int main(int, char const**)
 {
     // Create the main window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+    sf::RenderWindow window(sf::VideoMode(800, 800), "SFML window");
 
     // Set the Icon
     sf::Image icon;
@@ -37,6 +118,8 @@ int main(int, char const**)
     if (!texture.loadFromFile(resourcePath() + "cute_image.jpg")) {
         return EXIT_FAILURE;
     }
+    
+    
     sf::Sprite sprite(texture);
 
     // Create a graphical text to display
@@ -72,22 +155,15 @@ int main(int, char const**)
             if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
                 window.close();
             }
+            
+            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::R) {
+                render(window, sprite, text);
+            }
         }
 
-        // Clear screen
-        window.clear();
         
-        
-
-        // Draw the sprite
-        window.draw(sprite);
-
-        // Draw the string
-        window.draw(text);
-
-        // Update the window
-        window.display();
     }
 
     return EXIT_SUCCESS;
 }
+
